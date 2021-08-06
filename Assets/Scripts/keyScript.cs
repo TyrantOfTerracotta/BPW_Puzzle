61b9c5f832dd393451eeee2ce20f27684b54a02d
TyrantOfTerracotta <66208966+TyrantOfTerracotta@users.noreply.github.com>
2021-08-06 21:32:46 +0200

    Key Assets
    Script, materials, prefabs voor de puzzle Keys

--- Assets/Scripts/keyScript.cs
+++ Assets/Scripts/keyScript.cs
@@ -5,15 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class keyScript : MonoBehaviour
 {
-    [SerializeField] public GameObject key;
+    [SerializeField] public GameObject barrier;
+    [SerializeField] public GameObject gateOrb;
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
             FindObjectOfType<AudioManager>().Play("KeyFound");
-            Debug.Log("Picked one of the three keys!");
-            Destroy(key.gameObject);
+            Debug.Log("Picked one of the three keys! The matching door dissolved!");
+            Destroy(barrier.gameObject);
+            Destroy(gateOrb.gameObject);
+            Destroy(this.gameObject);
         }
     }
 }
