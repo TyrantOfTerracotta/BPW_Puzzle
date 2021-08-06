f5396eb558bfad2e584239788147ed385dd34ebb
TyrantOfTerracotta <66208966+TyrantOfTerracotta@users.noreply.github.com>
2021-08-06 22:59:49 +0200

    GoldPuzzle-CorrectAnswer
    Now able to destroy the correct answer box, to get to the hidden gold key.

---
+++ Assets/Scripts/goldScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goldScript : MonoBehaviour
+{
+    public GameObject goldRespawnPoint;
+    public GameObject Player;
+
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Projectile")
+        {
+            FindObjectOfType<AudioManager>().Play("PlayerDeath");
+
+            Player.transform.position = goldRespawnPoint.transform.position;
+            //SceneManager.LoadScene("Defeat");            
+            Debug.Log("Wrong answer!");
+            //Destroy(gameObject);
+        }
+    }
+}
